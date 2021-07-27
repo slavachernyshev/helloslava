ee381a452732f11eb9394c6330cc0d03a5a87edb
slavachernyhsev <chernyhsev@sinfores.ru>
2021-07-27 15:48:09 +0300

    Deleted docs folder

--- docs/_nuxt/static/1627389022/posts/5/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fhelloslava\u002F_nuxt\u002Fstatic\u002F1627389022",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002F5",config:{_app:{basePath:"\u002Fhelloslava\u002F",assetsPath:"\u002Fhelloslava\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"cc2c6e24"}}}}(null));
