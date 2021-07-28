3518a7b187ab26681903d3b5bb24bb01e6e97110
slavachernyhsev <chernyhsev@sinfores.ru>
2021-07-28 10:08:40 +0300

    Added Note component

---
+++ components/global/Note.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="text-sm text-gray-600 bg-gray-50 p-4 sm:p-6 rounded-none sm:rounded-lg -mx-4 sm:mx-0 space-y-2">
+    <div class="uppercase font-bold tracking-wide text-xs">{{ label }}</div>
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Note',
+
+  props: {
+    label: {
+      type: String,
+      default: 'Примечание'
+    }
+  }
+}
+</script>
