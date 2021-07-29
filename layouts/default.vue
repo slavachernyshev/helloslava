ab9214407262ec9e1d41a38634aebb3d3a150df7
slavachernyhsev <chernyhsev@sinfores.ru>
2021-07-29 10:43:09 +0300

    Mailchimp 2

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,16 +6,8 @@
 
 <script>
 export default {
-  mounted () {
-    this.setupMailchimpPopup()
-  },
-
-  methods: {
-    setupMailchimpPopup () {
-      let chimpPopupLoader = document.createElement("script")
-      chimpPopupLoader.src = 'https://chimpstatic.com/mcjs-connected/js/users/6c667c7166723a7e5c304db16/d739af95ab30674873b4f7a29.js'
-      document.head.appendChild(chimpPopupLoader)
-    }
+  head () {
+    __dangerouslyDisableSanitizers: ['script']
   }
 }
 </script>
