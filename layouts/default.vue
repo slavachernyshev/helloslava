87b243e32aa35358d1bc1fad54c37d87f509ee77
slavachernyhsev <chernyhsev@sinfores.ru>
2021-07-29 10:34:29 +0300

    Mailchimp test form

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,5 +4,21 @@
   </div>
 </template>
 
+<script>
+export default {
+  mounted () {
+    this.setupMailchimpPopup()
+  },
+
+  methods: {
+    setupMailchimpPopup () {
+      let chimpPopupLoader = document.createElement("script")
+      chimpPopupLoader.src = 'https://chimpstatic.com/mcjs-connected/js/users/6c667c7166723a7e5c304db16/d739af95ab30674873b4f7a29.js'
+      document.head.appendChild(chimpPopupLoader)
+    }
+  }
+}
+</script>
+
 <style>
 </style>
