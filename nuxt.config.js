cc1757bce0513e4032e01e0dead05e1c00e2e376
slavachernyhsev <chernyhsev@sinfores.ru>
2021-07-29 10:48:31 +0300

    Mailchimp 5

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,10 @@ export default {
     ],
     __dangerouslyDisableSanitizers: ['script'],
     script: [
-      { innerHTML: '!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/6c667c7166723a7e5c304db16/d739af95ab30674873b4f7a29.js");', id="mcjs" }
+      {
+        innerHTML: '!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/6c667c7166723a7e5c304db16/d739af95ab30674873b4f7a29.js");',
+        id: "mcjs" 
+      }
     ]
   },
 
